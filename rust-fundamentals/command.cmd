d5a07cc615153fe7135b8d5303a339a7a4bc5485
Osama AbuSitta <osama.abusitta@gmail.com>
2022-06-25 00:18:04 +0400

    initial commit

---
+++ rust-fundamentals/command.cmd
@@ -0,0 +1,8 @@
+REM initiate project 
+cargo init
+
+REM run 
+cargo run
+
+REM run without info 
+cargo run -q
---
+++ rust-fundamentals/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+  //first();
+  //second();
+  //match_expressions();
+  array();
+}
+
+fn array(){
+    let items:[i32;3] = [1,2,3];
+    print!("{:?}",items);
+}
+
+fn match_expressions(){
+    let mut total = 25;
+    let mut free_shipping = false;
+
+   total = match free_shipping {
+       true =>  total + 0 ,
+       false => total + 5
+    };
+
+    match total {
+        1 => print!("1"),
+        2 => print!("2"),
+        _ => print!("default")
+    }
+
+    print!("Total: {:?}",total);
+
+}
+
+fn first() {
+    let result = add(10, 10);
+    println!("{result}");
+    println!("{} {}", result, true);
+    println!("{0} {0}", result);
+
+    println!("{:?}", result);
+
+    fn add(num_one: i32, num_two: i32) -> i32 {
+        num_one + num_two
+    }
+}
+
+
+fn second(){
+    if 5 > 5 {
+        print!("true");
+    }
+    else {
+      print!("false");
+    }
+}
